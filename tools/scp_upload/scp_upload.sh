2d1efbb52712c31dd73fdc294c99c9cf0c4f567a
Gitefu <out.grow.1028@gmail.com>
2025-06-25 20:51:07 +0900

    リモート書き込み実装中

---
+++ tools/scp_upload/scp_upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# $1: elfファイルのパス
+# $2: リモートユーザー名
+# $3: リモートホスト
+# $4: リモート側の保存パス
+
+set -e
+
+LOCAL_ELF="$1"
+REMOTE_USER="$2"
+REMOTE_HOST="$3"
+REMOTE_PATH="$4"
+
+echo "Uploading $LOCAL_ELF to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+scp "$LOCAL_ELF" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
