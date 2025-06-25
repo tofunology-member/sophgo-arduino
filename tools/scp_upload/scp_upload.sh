a6e768ad191307213f56e5f69b67bffaeeb37eae
Gitefu <out.grow.1028@gmail.com>
2025-06-26 05:00:10 +0900

    リモート書き込みの仕組みを修正

--- tools/scp_upload/scp_upload.sh
+++ tools/scp_upload/scp_upload.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# $1: elfファイルのパス
-# $2: リモートユーザー名
-# $3: リモートホスト
-# $4: リモート側の保存パス
-
 set -e
 
 LOCAL_ELF="$1"
-REMOTE_USER="$2"
-REMOTE_HOST="$3"
-REMOTE_PATH="$4"
+READELF="$2"
+REMOTE_PATH="$3"
 
-echo "Uploading $LOCAL_ELF to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
-whoami
-sleep 2
-sshpass -p "milkv" scp -O "$LOCAL_ELF" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+PASSWD=$($READELF $LOCAL_ELF -p .scp_passwd | grep -oP '\]\s*\K.*' | tr -d '\n')
+REMOTE_USER=$($READELF $LOCAL_ELF -p .scp_username | grep -oP '\]\s*\K.*' | tr -d '\n')
+REMOTE_HOST=$($READELF $LOCAL_ELF -p .scp_hostname | grep -oP '\]\s*\K.*' | tr -d '\n')
+
+echo "Uploading $LOCAL_ELF to $REMOTE_USER@$:$REMOTE_PATH"
+sshpass -p $PASSWD scp -O "$LOCAL_ELF" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
